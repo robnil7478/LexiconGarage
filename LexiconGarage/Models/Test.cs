ede346f4060a770bcc29d11994f2142d6529d53e
robnil7478 <robertnilsson747@hotmail.com>
2016-10-16 21:46:43 +0200

    En test commit

---
+++ LexiconGarage/Models/Test.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace LexiconGarage.Models
+{
+    public class Test
+    {
+        public int MyProperty { get; set; }
+    }
+}
