0e13325530f80e11fcb7f025b8fbf01549edd549
Annika Nordqvist <annika_nqt@hotmail.com>
2016-10-17 14:11:18 +0200

    Add Range for Vehicle int properties. Remove ParkingTime from Create and Edit views. Remove Some properties from Index list.

--- LexiconGarage/Models/Vehicle.cs
+++ LexiconGarage/Models/Vehicle.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
@@ -19,9 +20,11 @@ namespace LexiconGarage.Models {
         public string RegNo { get; set; }
         public ConsoleColor Color { get; set; }
         public DateTime ParkingTime { get; set; } //DateTime2 ?? 
+        [Range(0, int.MaxValue, ErrorMessage = "Antalet hjul måste vara >= 0")]
         public int NumberOfWheels { get; set; }
         public string Brand { get; set; }
         public string Model { get; set; }
+        [Range(0, int.MaxValue, ErrorMessage = "Vikten i kg, måste vara >= 0")]
         public int Weight { get; set; }
     }
 }
