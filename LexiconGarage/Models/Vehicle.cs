0d9eb431d99434268c3cf7a8e7358184c9b5bac9
RobNil7478 <robertnilsson747@hotmail.com>
2016-10-17 14:40:45 +0200

    Ändrar namn för rubriker

--- LexiconGarage/Models/Vehicle.cs
+++ LexiconGarage/Models/Vehicle.cs
@@ -2,8 +2,9 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
-
+using System.ComponentModel.DataAnnotations;
 namespace LexiconGarage.Models {
+
     public enum VehicleType {
         MotorCycle,
         Car,
@@ -12,16 +13,45 @@ namespace LexiconGarage.Models {
         Boat
     }
 
+    
+
     public class Vehicle {
+        private DateTime? dateCreated = null;
 
         public int Id { get; set; }
+
+        [Display(Name = "Fordonstyp")]
         public VehicleType Type { get; set; }
+
+        [Display(Name = "Registreringsnummer")]
         public string RegNo { get; set; }
+
+        [Display(Name = "Färg")]
         public ConsoleColor Color { get; set; }
-        public DateTime ParkingTime { get; set; } //DateTime2 ?? 
+
+        [Display(Name = "Parkeringstid")]
+        public DateTime  ParkingTime {          //DateTime2 ??
+            get
+            {
+                return this.dateCreated.HasValue
+                   ? this.dateCreated.Value
+                   : DateTime.Now;
+            }
+
+            set { this.dateCreated = value; }
+
+        } 
+
+        [Display(Name = "Antal hjul")]
         public int NumberOfWheels { get; set; }
+
+        [Display(Name = "Märke")]
         public string Brand { get; set; }
+
+        [Display(Name = "Modell")]
         public string Model { get; set; }
+
+        [Display(Name = "Vikt")]
         public int Weight { get; set; }
     }
 }
